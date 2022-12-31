005c2bdf66792771d692f04a4f20f45eb3d42edc
Pratyush Gupta <pg0623733@gmail.com>
2022-12-31 15:44:37 +0530

    Added routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } =require('./config/serverConfig');
+
+const apiRoutes = require('./routes/index');
 
 const app = express();
-const { PORT } =require('./config/serverConfig');
 const prepareAndStartServer = () =>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',apiRoutes);
+
     app.listen(PORT,() =>{
         console.log(`Server started at ${PORT} `);
     })
 }
 
-prepareAndStartServer();
+prepareAndStartServer(); 
